8a854fbd6c1f228b2b6416f056ee1b1aecd93e90
Sam <skb227@lehigh.edu>
2026-02-20 01:42:36 +0000

    halfway break

---
+++ benchmark/hello_OLD.cc
@@ -0,0 +1,16 @@
+#include <memory>
+#include <remus/remus.h>
+
+int main(int argc, char **argv) {
+  remus::INIT();
+
+  // Configure and parse the arguments
+  auto args = std::make_shared<remus::ArgMap>();
+  args->import(remus::ARGS);
+  args->parse(argc, argv);
+  if (args->bget(remus::HELP)) {
+    args->usage();
+    return 0;
+  }
+  args->report_config();
+}
